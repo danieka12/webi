61435104d3fd594f2a8ca70bd357e1c0581e0467
danieka12 <elangindra15@protonmail.com>
2021-12-06 12:54:15 +0700

    Redesign the course material

--- resources/views/components/homepage/course.blade.php
+++ resources/views/components/homepage/course.blade.php
@@ -5,83 +5,9 @@
         <p>{{ $desc }}</p>
     </div>
     <div class="row">
-        <div class="col-lg-4 col-md-6 wow" data-wow-offset="150">
-            <a href="#0" class="grid_item">
-                <figure class="block-reveal">
-                    <div class="block-horizzontal"></div>
-                    <img src="http://via.placeholder.com/800x533/ccc/fff/course_1.jpg" class="image-fluid" alt="" />
-                        <div class="info">
-                            <small><i class="ti-layers"></i>15 Programmes</small>
-                            <h3>Arts and Humanities</h3>
-                        </div>
-                </figure>
-            </a>
-        </div>
-        <!-- /grid_item -->
-        <div class="col-lg-4 col-md-6 wow" data-wow-offset="150">
-            <a href="#0" class="grid_item">
-                <figure class="block-reveal">
-                    <div class="block-horizzontal"></div>
-                    <img src="http://via.placeholder.com/800x533/ccc/fff/course_2.jpg" class="image-fluid" alt="" />
-                        <div class="info">
-                            <small><i class="ti-layers"></i>23 Programmes</small>
-                            <h3>Engineering</h3>
-                        </div>
-                </figure>
-            </a>
-        </div>
-        <!-- /grid_item -->
-        <div class="col-lg-4 col-md-6 wow" data-wow-offset="150">
-            <a href="#0" class="grid_item">
-                <figure class="block-reveal">
-                    <div class="block-horizzontal"></div>
-                    <img src="http://via.placeholder.com/800x533/ccc/fff/course_3.jpg" class="image-fluid" alt="" />
-                        <div class="info">
-                            <small><i class="ti-layers"></i>23 Programmes</small>
-                            <h3>Architecture</h3>
-                        </div>
-                </figure>
-            </a>
-        </div>
-        <!-- /grid_item -->
-        <div class="col-lg-4 col-md-6 wow" data-wow-offset="150">
-            <a href="#0" class="grid_item">
-                <figure class="block-reveal">
-                    <div class="block-horizzontal"></div>
-                    <img src="http://via.placeholder.com/800x533/ccc/fff/course_4.jpg" class="image-fluid" alt="" />
-                        <div class="info">
-                            <small><i class="ti-layers"></i>23 Programmes</small>
-                            <h3>Science and Biology</h3>
-                        </div>
-                </figure>
-            </a>
-        </div>
-        <!-- /grid_item -->
-        <div class="col-lg-4 col-md-6 wow" data-wow-offset="150">
-            <a href="#0" class="grid_item">
-                <figure class="block-reveal">
-                    <div class="block-horizzontal"></div>
-                    <img src="http://via.placeholder.com/800x533/ccc/fff/course_5.jpg" class="image-fluid" alt="" />
-                        <div class="info">
-                            <small><i class="ti-layers"></i>23 Programmes</small>
-                            <h3>Law and Criminology</h3>
-                        </div>
-                </figure>
-            </a>
-        </div>
-        <!-- /grid_item -->
-        <div class="col-lg-4 col-md-6 wow" data-wow-offset="150">
-            <a href="#0" class="grid_item">
-                <figure class="block-reveal">
-                    <div class="block-horizzontal"></div>
-                    <img src="http://via.placeholder.com/800x533/ccc/fff/course_6.jpg" class="image-fluid" alt="" />
-                        <div class="info">
-                            <small><i class="ti-layers"></i>23 Programmes</small>
-                            <h3>Medical</h3>
-                        </div>
-                </figure>
-            </a>
-        </div>
+     @foreach ($categories as $category)
+         @include('components.miniComponents.category-course-card', ['title' => $category['title'], 'metaTitle' => $category['metaTitle'], 'previewImage' => $category['previewImage'], 'href' => $category['href']])
+     @endforeach
         <!-- /grid_item -->
     </div>
     <!-- /row -->
---
+++ resources/views/components/miniComponents/category-course-card.blade.php
@@ -0,0 +1,12 @@
+<div class="col-lg-4 col-md-6 wow" data-wow-offset="150">
+    <a href={{ $href }} class="grid_item">
+        <figure class="block-reveal">
+            <div class="block-horizzontal"></div>
+            <img src={{ $previewImage }} class="image-fluid" alt="" />
+                <div class="info">
+                    <small><i class="ti-layers"></i>{{ $metaTitle }}</small>
+                    <h3>{{ $title }}</h3>
+                </div>
+        </figure>
+    </a>
+</div>
