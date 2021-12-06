58ca1f946e39b41cf818a2e09f388d45df319a50
danieka12 <elangindra15@protonmail.com>
2021-12-06 13:07:06 +0700

    Redesign the news

--- resources/views/components/homepage/news-and-event.blade.php
+++ resources/views/components/homepage/news-and-event.blade.php
@@ -6,69 +6,13 @@
             <p>{{ $desc }}</p>
         </div>
         <div class="row">
-            <div class="col-lg-6">
-                <a class="box_news" href="#0">
-                    <figure>
-                        <img src="http://via.placeholder.com/500x333/ccc/fff/news_home_1.jpg" alt="" />
-                            <figcaption><strong>28</strong>Dec</figcaption>
-                    </figure>
-                    <ul>
-                        <li>Mark Twain</li>
-                        <li>20.11.2017</li>
-                    </ul>
-                    <h4>Pri oportere scribentur eu</h4>
-                    <p>Cu eum alia elit, usu in eius appareat, deleniti sapientem honestatis eos ex. In ius esse ullum vidisse....</p>
-                </a>
-            </div>
-            <!-- /box_news -->
-            <div class="col-lg-6">
-                <a class="box_news" href="#0">
-                    <figure>
-                        <img src="http://via.placeholder.com/500x333/ccc/fff/news_home_2.jpg" alt="" />
-                            <figcaption><strong>28</strong>Dec</figcaption>
-                    </figure>
-                    <ul>
-                        <li>Jhon Doe</li>
-                        <li>20.11.2017</li>
-                    </ul>
-                    <h4>Duo eius postea suscipit ad</h4>
-                    <p>Cu eum alia elit, usu in eius appareat, deleniti sapientem honestatis eos ex. In ius esse ullum vidisse....</p>
-                </a>
-            </div>
-            <!-- /box_news -->
-            <div class="col-lg-6">
-                <a class="box_news" href="#0">
-                    <figure>
-                        <img src="http://via.placeholder.com/500x333/ccc/fff/news_home_3.jpg" alt="" />
-                            <figcaption><strong>28</strong>Dec</figcaption>
-                    </figure>
-                    <ul>
-                        <li>Luca Robinson</li>
-                        <li>20.11.2017</li>
-                    </ul>
-                    <h4>Elitr mandamus cu has</h4>
-                    <p>Cu eum alia elit, usu in eius appareat, deleniti sapientem honestatis eos ex. In ius esse ullum vidisse....</p>
-                </a>
-            </div>
-            <!-- /box_news -->
-            <div class="col-lg-6">
-                <a class="box_news" href="#0">
-                    <figure>
-                        <img src="http://via.placeholder.com/500x333/ccc/fff/news_home_4.jpg" alt="" />
-                            <figcaption><strong>28</strong>Dec</figcaption>
-                    </figure>
-                    <ul>
-                        <li>Paula Rodrigez</li>
-                        <li>20.11.2017</li>
-                    </ul>
-                    <h4>Id est adhuc ignota delenit</h4>
-                    <p>Cu eum alia elit, usu in eius appareat, deleniti sapientem honestatis eos ex. In ius esse ullum vidisse....</p>
-                </a>
-            </div>
-            <!-- /box_news -->
+           @foreach ($newsAndEvents as $newsAndEvent)
+               @include('components.miniComponents.news-card', ['title' => $newsAndEvent['title'], 'desc' => $newsAndEvent['desc'], 'author' => $newsAndEvent['author'], 'dateTime' => $newsAndEvent['dateTime'], 'href' => $newsAndEvent['href'], 'meta' => $newsAndEvent['meta']])
+           <!-- /box_news -->
+           @endforeach
         </div>
         <!-- /row -->
-        <p class="btn_home_align"><a href="blog.html" class="btn_1 rounded">View all news</a></p>
+        <p class="btn_home_align"><a href="blog.html" class="btn_1 rounded">{{ isset($viewMore) ? $viewMore : 'Lihat semua berita' }}</a></p>
     </div>
     <!-- /container -->
 </div>
---
+++ resources/views/components/miniComponents/news-card.blade.php
@@ -0,0 +1,14 @@
+<div class="col-lg-6">
+    <a class="box_news" href={{ $href }}>
+        <figure>
+            <img src={{ $meta['imagePreview'] }} alt="" />
+                <figcaption><strong>{{ $meta['date'] }}</strong>{{ $meta['month'] }}</figcaption>
+        </figure>
+        <ul>
+            <li>{{ $author }}</li>
+            <li>{{ $dateTime }}</li>
+        </ul>
+        <h4>{{ $title }}</h4>
+        <p>{{ $desc }}</p>
+    </a>
+</div>
