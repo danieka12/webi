660089a784d7793b8e82ffd9740c2eedbf11e73c
danieka12 <elangindra15@protonmail.com>
2021-12-06 12:05:17 +0700

    Removing some unusable component in header

---
+++ resources/views/components/miniComponents/search-menu.blade.php
@@ -0,0 +1,8 @@
+<div class="search-overlay-menu">
+    <span class="search-overlay-close"><span class="closebt"><i class="ti-close"></i></span></span>
+    <form role="search" id="searchform" method="get">
+        <input value="" name="q" type="search" placeholder="Search..." />
+        <button type="submit"><i class="icon_search"></i>
+        </button>
+    </form>
+</div><!-- End Search Menu -->
---
+++ resources/views/components/miniComponents/top-menu.blade.php
@@ -0,0 +1,5 @@
+<ul id="top_menu">
+    {{-- <li><a href="login.html" class="login">Login</a></li> --}}
+    {{-- <li><a href="#0" class="search-overlay-menu-btn">Search</a></li> --}}
+    <li class="hidden_tablet"><a href="admission.html" class="btn_1 rounded">Masuk</a></li>
+</ul>
