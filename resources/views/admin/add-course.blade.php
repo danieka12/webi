5932b934e30a9e5e5a5c59b09e65f8b2185db711
danieka12 <elangindra15@protonmail.com>
2021-12-21 15:01:15 +0700

    Adding course material

--- resources/views/admin/add-course.blade.php
+++ resources/views/admin/add-course.blade.php
@@ -12,47 +12,14 @@
 			<div class="row">
 				<div class="col-md-6">
 					<div class="form-group">
-						<label>Course title</label>
-						<input type="text" class="form-control" placeholder="Course title">
+						<label>Judul Materi</label>
+						<input type="text" class="form-control" placeholder="Judul Materi">
 					</div>
 				</div>
 				<div class="col-md-6">
 					<div class="form-group">
-						<label>Course price</label>
+						<label>Durasi Materi</label>
 						<input type="text" class="form-control" placeholder="Course category">
-					</div>
-				</div>
-			</div>
-			<!-- /row-->
-			<div class="row">
-				<div class="col-md-6">
-					<div class="form-group">
-						<label>Course start</label>
-						<input type="text" class="form-control date-pick" placeholder="Course start">
-					</div>
-				</div>
-				<div class="col-md-6">
-					<div class="form-group">
-						<label>Course expire</label>
-						<input type="email" class="form-control date-pick" placeholder="Your email">
-					</div>
-				</div>
-			</div>
-			<!-- /row-->
-			<div class="row">
-				<div class="col-md-6">
-					<div class="form-group">
-						<label>Teacher name</label>
-						<input type="text" class="form-control" placeholder="Course teacher">
-					</div>
-				</div>
-			</div>
-			<!-- /row-->
-			<div class="row">
-				<div class="col-md-12">
-					<div class="form-group">
-						<label>Course picture</label>
-						<form action="/file-upload" class="dropzone" ></form>
 					</div>
 				</div>
 			</div>
@@ -84,49 +51,7 @@
 			<!-- /row-->
 		</div>
 		<!-- /box_general-->
-		
-		<div class="box_general padding_bottom">
-			<div class="header_box version_2">
-				<h2><i class="fa fa-video-camera"></i>Videos</h2>
-			</div>
-			<div class="row">
-				<div class="col-md-12">
-					<h6>Item</h6>
-					<table id="pricing-list-container" style="width:100%;">
-						<tr class="pricing-list-item">
-							<td>
-								<div class="row">
-									<div class="col-md-4">
-										<div class="form-group">
-											<input type="text" class="form-control" placeholder="Video title">
-										</div>
-									</div>
-									<div class="col-md-3">
-										<div class="form-group">
-											<input type="text" class="form-control" placeholder="Video category">
-										</div>
-									</div>
-									<div class="col-md-3">
-										<div class="form-group">
-											<input type="text" class="form-control"  placeholder="Video URL">
-										</div>
-									</div>
-									<div class="col-md-2">
-										<div class="form-group">
-											<a class="delete" href="#"><i class="fa fa-fw fa-remove"></i></a>
-										</div>
-									</div>
-								</div>
-							</td>
-						</tr>
-					</table>
-					<a href="#0" class="btn_1 gray add-pricing-list-item"><i class="fa fa-fw fa-plus-circle"></i>Add Item</a>
-					</div>
-			</div>
-			<!-- /row-->
-		</div>
-		<!-- /box_general-->
-		<p><a href="#0" class="btn_1 medium">Save</a></p>
+		<p><a href="#0" class="btn_1 medium">Simpan Materi</a></p>
 	  </div>
 	  <!-- /.container-fluid-->
    	</div>
